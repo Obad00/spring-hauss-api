b9d5858c3f0c233874d8a03ebf82ed174a289def
Obad00 <dabodabo9064@gmail.com>
2024-10-08 15:47:04 +0000

    Amelioration

--- src/main/java/com/reservation/dto/AuthResponse.java
+++ src/main/java/com/reservation/dto/AuthResponse.java
@@ -2,17 +2,24 @@ package com.reservation.dto;
 
 import com.reservation.entity.User;
 
+import com.reservation.enums.RoleUtilisateur; // Importez l'enum
+
 public class AuthResponse {
-    
     private String token;
+    private RoleUtilisateur role; // Utilisez RoleUtilisateur comme type
     private User user;
 
     public AuthResponse(String token, User user) {
         this.token = token;
         this.user = user;
+        this.role = user.getRole(); // Assurez-vous que cette méthode existe
     }
-
     // Getters et setters
+
+
+    public RoleUtilisateur getRole() {
+        return role; // Retourne le rôle sous forme d'enum
+    }
 
     public String getToken() {
         return token;
