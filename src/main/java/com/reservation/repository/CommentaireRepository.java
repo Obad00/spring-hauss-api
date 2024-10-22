185b823415d9d7b1ef5e097ae24065d5627a7fbf
Obad00 <dabodabo9064@gmail.com>
2024-10-22 13:27:08 +0000

    Mise en place de la relation des commentaire avec un logement

--- src/main/java/com/reservation/repository/CommentaireRepository.java
+++ src/main/java/com/reservation/repository/CommentaireRepository.java
@@ -2,6 +2,12 @@ package com.reservation.repository;
 
 import com.reservation.entity.Commentaire;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface CommentaireRepository extends JpaRepository<Commentaire, Long> {
+    // Récupérer tous les commentaires pour un logement spécifique
+    List<Commentaire> findByLogementId(Long logementId);
 }
